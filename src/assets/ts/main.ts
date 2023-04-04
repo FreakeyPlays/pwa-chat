d527f20190f82459e40dc20322624aecba094845
Chris Merck <chrismerck@gmx.de>
2023-04-04 12:26:31 +0200

    feat(main): updated

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,3 +1,22 @@
+import { formValidator } from './_validation/form.validator.js';
 import { Router } from './router.js';
-const router = new Router();
 
+const router = new Router();
+const validator = new formValidator();
+
+document.body.addEventListener('spaContentLoaded', e => {
+  const form: HTMLElement | null = document.getElementById('form');
+
+  if (form == null) return;
+
+  switch (form.dataset.formId) {
+    case 'login':
+      validator.setup(form);
+      break;
+    case 'signup':
+      validator.setup(form);
+      break;
+    default:
+      break;
+  }
+});
